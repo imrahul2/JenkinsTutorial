492308218d81e6fb9d6ac4279e1131a03b45699e
Rahul Lodha <60583499+imrahul2@users.noreply.github.com>
2022-08-02 16:38:40 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld{
+    public static void main(String [] s){
+        System.out.println(x:"Hello all");
+        for(int i=0;i<20;i++){
+            System.out.println("Value of I: " +i);
+        }
+    }
+}
