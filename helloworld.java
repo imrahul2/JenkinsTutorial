bd5533387610b3109ce6271a6674f8115b12cccc
Rahul Lodha <60583499+imrahul2@users.noreply.github.com>
2022-08-02 16:49:15 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld{
     public static void main(String [] s){
-        System.out.println(x:"Hello all");
+        System.out.println("Hello all");
         for(int i=0;i<20;i++){
             System.out.println("Value of I: " +i);
         }
